b7c84ed282c18cefb3b953747c22bba3ea716b21
Gunottam Maini <138522576+gunottam@users.noreply.github.com>
2025-11-22 20:31:25 +0530

    Update read.c

--- 13sharedmemory/read.c
+++ 13sharedmemory/read.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
+int main() {
+    key_t key = ftok("shmfile", 65);
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+
+    char *str = (char*) shmat(shmid, NULL, 0);
+
+    printf("Data read: %s\n", str);
+
+    shmdt(str);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
+}
