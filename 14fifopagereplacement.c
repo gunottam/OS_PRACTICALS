d35a7641c38208644bcdf62b8d08763d4e8e325e
Gunottam Maini <138522576+gunottam@users.noreply.github.com>
2025-11-22 20:32:25 +0530

    Add FIFO page replacement algorithm
    Implement FIFO page replacement algorithm in C.

---
+++ 14fifopagereplacement.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int pages[50], frame[10], n, f, i, j, k, flag, faults=0;
+
+    printf("Enter number of pages: ");
+    scanf("%d", &n);
+
+    printf("Enter page sequence: ");
+    for(i=0;i<n;i++)
+        scanf("%d",&pages[i]);
+
+    printf("Enter number of frames: ");
+    scanf("%d",&f);
+
+    for(i=0;i<f;i++)
+        frame[i] = -1;
+
+    k = 0;
+
+    for(i=0;i<n;i++){
+        flag = 0;
+
+        for(j=0;j<f;j++){
+            if(frame[j] == pages[i]){
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 0){
+            frame[k] = pages[i];
+            k = (k+1) % f;
+            faults++;
+        }
+    }
+
+    printf("\nPage Faults: %d\n", faults);
+    return 0;
+}
