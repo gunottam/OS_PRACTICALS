8abb6a5e2a641720555f592470c229a76c531947
Gunottam Maini <138522576+gunottam@users.noreply.github.com>
2025-11-24 12:57:42 +0530

    Update writer.c

--- 11fifoPipe/writer.c
+++ 11fifoPipe/writer.c
@@ -1,52 +1,20 @@
-// write.c
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <errno.h>
+#include <sys/stat.h>
 
 int main() {
-    const char *fifoPath = "/tmp/myfifo";
-    int fd;
-
-    // Create FIFO if not exists
-    if (mkfifo(fifoPath, 0666) == -1) {
-        if (errno != EEXIST) {
-            perror("mkfifo");
-            exit(EXIT_FAILURE);
-        }
-    }
-
     char msg[100];
 
-    printf("Enter message to write: ");
+    mkfifo("myfifo", 0666);
+
+    printf("Enter message: ");
     fgets(msg, sizeof(msg), stdin);
 
-    // remove newline at end (optional cleaner display)
-    msg[strcspn(msg, "\n")] = '\0';
+    int fd = open("myfifo", O_WRONLY);
+    write(fd, msg, sizeof(msg));
 
-    int len = strlen(msg);
-
-    // Open FIFO for writing only
-    fd = open(fifoPath, O_WRONLY);
-    if (fd == -1) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
-
-    // Write data to FIFO
-    int written = write(fd, msg, len);
-    if (written == -1) {
-        perror("write");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Write data:%s\n", msg);
-    printf("No of bytes:%d\n", written);
+    printf("Message written: %s\n", msg);
 
     close(fd);
     return 0;
