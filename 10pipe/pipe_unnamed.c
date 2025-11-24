3db3e26b44825bfc5fc865ed827e01e3047e8b38
Gunottam Maini <138522576+gunottam@users.noreply.github.com>
2025-11-24 12:49:26 +0530

    Update pipe_unnamed.c

--- 10pipe/pipe_unnamed.c
+++ 10pipe/pipe_unnamed.c
@@ -1,72 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
 
 int main() {
-    int fd[2];  // fd[0] = read end, fd[1] = write end
+    int fd[2];
+    pipe(fd);
 
-    if (pipe(fd) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
+    int pid = fork();
 
-    pid_t pid = fork();
-
-    if (pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    // Parent Process (Writer)
-    if (pid > 0) {
-        close(fd[0]);  // close read end
-
-        char msg[100];
-
-        printf("Enter message to send: ");
-        if (fgets(msg, sizeof(msg), stdin) == NULL) {
-            perror("fgets");
-            close(fd[1]);
-            exit(EXIT_FAILURE);
-        }
-
-        // remove trailing newline if present
-        msg[strcspn(msg, "\n")] = '\0';
-
-        int len = strlen(msg);
-
-        // Write to pipe
-        int written = write(fd[1], msg, len);
-        if (written == -1) {
-            perror("write");
-            close(fd[1]);
-            exit(EXIT_FAILURE);
-        }
-
-        printf("Write data:%s\n", msg);
-        printf("No of bytes:%d\n", written);
-
+    if(pid > 0) { 
+        // Parent writes
+        close(fd[0]);
+        char msg[50];
+        printf("Enter message: ");
+        fgets(msg, sizeof(msg), stdin);
+        write(fd[1], msg, sizeof(msg));
         close(fd[1]);
-    }
-    // Child Process (Reader)
-    else {
-        close(fd[1]);  // close write end
-
-        char buffer[100];
-        int n = read(fd[0], buffer, sizeof(buffer)-1);
-        if (n == -1) {
-            perror("read");
-            close(fd[0]);
-            exit(EXIT_FAILURE);
-        }
-
-        buffer[n] = '\0';
-
-        printf("Message Written:%s\n", buffer);
-        printf("No of bytes:%d\n", n);
-
+    } 
+    else { 
+        // Child reads
+        close(fd[1]);
+        char buffer[50];
+        read(fd[0], buffer, sizeof(buffer));
+        printf("\nReceived: %s", buffer);
         close(fd[0]);
     }
 
