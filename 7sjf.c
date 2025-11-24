6cecb8a4ba004c35c5089a9b54f68c0c35d444aa
Gunottam Maini <138522576+gunottam@users.noreply.github.com>
2025-11-25 05:19:38 +0530

    Update 7sjf.c

--- 7sjf.c
+++ 7sjf.c
@@ -7,41 +7,53 @@ int main() {
 
     int pid[n], at[n], bt[n], ct[n], tat[n], wt[n], done[n];
 
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++) {
         printf("Enter PID, AT, BT: ");
-        scanf("%d %d %d",&pid[i],&at[i],&bt[i]);
-        done[i]=0;
+        scanf("%d %d %d", &pid[i], &at[i], &bt[i]);
+        done[i] = 0;
     }
 
-    int time=0, completed=0;
+    int time = 0, completed = 0;
 
-    while(completed < n) {
-        int idx=-1, minBT=9999;
+    printf("\n--- GANTT CHART ---\n");
+    printf("%d", time);   // starting time
 
-        // find shortest available process
-        for(int i=0;i<n;i++){
-            if(at[i] <= time && done[i]==0 && bt[i] < minBT){
+    while (completed < n) {
+        int idx = -1, minBT = 9999;
+
+        // find the shortest available job
+        for (int i = 0; i < n; i++) {
+            if (at[i] <= time && done[i] == 0 && bt[i] < minBT) {
                 minBT = bt[i];
                 idx = i;
             }
         }
 
-        if(idx == -1) { 
-            time++; 
-            continue; 
+        // CPU idle
+        if (idx == -1) {
+            time++;
+            printf(" --IDLE-- %d", time);
+            continue;
         }
 
+        // Execute process
+        printf(" --P%d-- ", pid[idx]);
+
         time += bt[idx];
-        ct[idx] = time;
+        printf("%d", time);
+
+        ct[idx]  = time;
         tat[idx] = ct[idx] - at[idx];
-        wt[idx] = tat[idx] - bt[idx];
+        wt[idx]  = tat[idx] - bt[idx];
         done[idx] = 1;
         completed++;
     }
 
-    printf("\nPID AT BT CT TAT WT\n");
-    for(int i=0;i<n;i++)
-        printf("%d  %d  %d  %d  %d  %d\n",pid[i],at[i],bt[i],ct[i],tat[i],wt[i]);
+    printf("\n\nPID  AT  BT  CT  TAT  WT\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d   %d   %d   %d   %d   %d\n",
+               pid[i], at[i], bt[i], ct[i], tat[i], wt[i]);
+    }
 
     return 0;
 }
