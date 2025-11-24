ceb8ea375449fbcc3b12393a8c0ef8da3056b850
Gunottam Maini <138522576+gunottam@users.noreply.github.com>
2025-11-25 05:26:35 +0530

    Update 8srtf.c

--- 8srtf.c
+++ 8srtf.c
@@ -7,33 +7,54 @@ int main() {
 
     int pid[n], at[n], bt[n], rt[n], ct[n], tat[n], wt[n];
 
-    for(int i=0;i<n;i++){
+    for(int i = 0; i < n; i++) {
         printf("Enter PID, AT, BT: ");
-        scanf("%d %d %d",&pid[i],&at[i],&bt[i]);
-        rt[i]=bt[i];
+        scanf("%d %d %d", &pid[i], &at[i], &bt[i]);
+        rt[i] = bt[i];
     }
 
-    int time=0, completed=0;
+    int time = 0, completed = 0;
+    int prev = -1;  // previous running process (for neat Gantt chart)
 
-    while(completed < n) {
-        int idx=-1, minRT=9999;
+    printf("\n--- GANTT CHART ---\n");
+    printf("%d", time);
 
-        for(int i=0;i<n;i++){
-            if(at[i] <= time && rt[i] > 0 && rt[i] < minRT){
+    while (completed < n) {
+
+        int idx = -1, minRT = 9999;
+
+        // select shortest remaining time process
+        for (int i = 0; i < n; i++) {
+            if (at[i] <= time && rt[i] > 0 && rt[i] < minRT) {
                 minRT = rt[i];
-                idx=i;
+                idx = i;
             }
         }
 
-        if(idx == -1){
+        // CPU idle
+        if (idx == -1) {
+            if (prev != -2) {  // print idle once
+                printf(" --IDLE-- ");
+                prev = -2;
+            }
             time++;
+            printf("%d", time);
             continue;
         }
 
+        // If process changes, print it
+        if (idx != prev) {
+            printf(" --P%d-- ", pid[idx]);
+            prev = idx;
+        }
+
+        // Execute 1 unit
         rt[idx]--;
         time++;
+        printf("%d", time);
 
-        if(rt[idx]==0){
+        // Process completed
+        if (rt[idx] == 0) {
             ct[idx] = time;
             tat[idx] = ct[idx] - at[idx];
             wt[idx] = tat[idx] - bt[idx];
@@ -41,9 +62,11 @@ int main() {
         }
     }
 
-    printf("\nPID AT BT CT TAT WT\n");
-    for(int i=0;i<n;i++)
-        printf("%d  %d  %d  %d  %d  %d\n",pid[i],at[i],bt[i],ct[i],tat[i],wt[i]);
+    printf("\n\nPID  AT  BT  CT  TAT  WT\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d   %d   %d   %d   %d   %d\n",
+               pid[i], at[i], bt[i], ct[i], tat[i], wt[i]);
+    }
 
     return 0;
 }
